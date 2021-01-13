6b9ae6114fd8b641918d2b881d3c0be9e750a0a9
Zukkari <stanislav.myshkovski@gmail.com>
2021-01-13 17:53:02 +0200

    Initial project setup

---
+++ src/test/java/io/github/zukkari/sectors/SectorsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.zukkari.sectors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SectorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
